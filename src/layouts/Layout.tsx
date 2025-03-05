db1f0e1e682b3c61d13bf2cdfbaaea181c4ff395
Stan <moscarstanley37@gmail.com>
2025-03-05 14:44:45 -0600

    feat: Enhance layout and product pages with headers and navigation links

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,9 +3,19 @@ import { Outlet } from "react-router-dom";
 export const Layout = () => {
   return (
     <>
-      <div>Desde Layout</div>
+      <header className="bg-slate-900">
+        <div className="mx-auto max-w-6xl py-10">
+            <h1 className="text-4xl font-extrabold text-white">
+                Administrador de Productos
+            </h1>
+        </div>
+      </header>
 
+
+    <main className="mt-10 mx-auto max-w-6xl p-10 bg-white shadow">
       <Outlet />
+    </main>
+
     </>
   );
 };
--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,6 +1,18 @@
+import { Link } from "react-router-dom";
 
 export const NewProduct = () => {
   return (
-    <div>NewProduct</div>
-  )
-}
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Registrar producto</h2>
+
+        <Link
+          to="/"
+          className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+        >
+          Volver a productos
+        </Link>
+      </div>
+    </>
+  );
+};
